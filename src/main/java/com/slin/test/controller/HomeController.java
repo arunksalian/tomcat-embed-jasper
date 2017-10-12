49f2722df0f02eaf3d983b4da5454f56a881634d
arunksalian <arun.salian@gmail.com>
2017-10-12 17:38:51 +0530

    added java files

---
+++ src/main/java/com/slin/test/controller/HomeController.java
@@ -0,0 +1,26 @@
+package com.slin.test.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@RequestMapping("/")
+public class HomeController {
+
+	@RequestMapping(value = "/home", method = RequestMethod.GET)
+	@ResponseBody
+	public String getHome() {
+		return "h;ii";
+	}
+
+	@RequestMapping(value = "/greetings", method = RequestMethod.GET)
+	public String greeting(@RequestParam(value = "name", required = false, defaultValue = "World") String name,
+			Model model) {
+		model.addAttribute("name", name);
+		return "greeting";
+	}
+}
